6bbe328d612b39496de8d0e7d3430f9cd940e8a2
qzsousa <nascimentopablo2018@gmail.com>
2026-06-02 22:31:17 -0300

    create page for login

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.database import create_db_and_tables
 from app.routers import usuarios, itens, chamado, auth
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -12,3 +13,11 @@ app.include_router(auth.router)
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5500", "http://127.0.0.1:5500"],  # origem do seu frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
